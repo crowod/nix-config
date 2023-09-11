44cb04775051fa7bbde22f1ac28a34f3e6d2fbbe
crowod <xiangcz929@gmail.com>
2023-09-11 22:28:11 +0800

    [overlay] vscode flags
    Signed-off-by: crowod <xiangcz929@gmail.com>

--- overlays/vscode/default.nix
+++ overlays/vscode/default.nix
@@ -1,5 +1,14 @@
 self: super: {
-  vscode = super.vscode.override {
+  vscode = super.vscode.overrideAttrs (oldAttrs: {
     commandLineArgs = "--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime";
-  };
+    preFixup = ''
+      gappsWrapperArgs+=(
+        # Add gio to PATH so that moving files to the trash works when not using a desktop environment
+        --prefix PATH : ${super.glib.bin}/bin
+        --add-flags '--enable-features=UseOzonePlatform --ozone-platform=wayland --enable-wayland-ime'
+        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
+        --add-flags ${super.lib.escapeShellArg super.commandLineArgs}
+      )
+    '';
+  });
 }
