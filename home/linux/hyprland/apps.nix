294c73cc4f7132a6cbe95ed26aeb198e13bc1ad8
crowod <xiangcz929@gmail.com>
2023-10-18 17:49:21 +0800

    update package version
    Signed-off-by: crowod <xiangcz929@gmail.com>

--- home/linux/hyprland/apps.nix
+++ home/linux/hyprland/apps.nix
@@ -1,8 +1,9 @@
-{ ... }: {
+{ pkgs-unstable, ... }: {
   programs = {
     # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
     google-chrome = {
       enable = true;
+      package = pkgs-unstable.google-chrome;
 
       commandLineArgs = [
         "--gtk-version=4"
