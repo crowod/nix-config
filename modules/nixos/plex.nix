2ba39cac49709d8ec7cd91a7a408a7401246e2ff
crowod <xiangcz929@gmail.com>
2024-12-01 20:01:49 +0800

    update plex
    Signed-off-by: crowod <xiangcz929@gmail.com>

--- modules/nixos/plex.nix
+++ modules/nixos/plex.nix
@@ -1,4 +1,4 @@
-{ username, lib,... }:{
+{ username, lib, pkgs,... }:{
   
   systemd.services.plex.serviceConfig.ProtectHome = lib.mkForce false;
 
@@ -7,5 +7,24 @@
     openFirewall = true;
     user = username;
     group = "users";
+    extraPlugins = [
+      (builtins.path {
+        name = "Hama.bundle";
+        path = pkgs.fetchFromGitHub {
+          owner = "ZeroQI";
+          repo = "Hama.bundle";
+          rev = "fb6a5689359c6630c0fcfec58f8e3533497fd977";
+          sha256 = "6xFYCg4wP1ZARKgOPxMZQlt4yHei9FwvqXI1fVHg1NA=";
+        };
+      })
+    ];
+    extraScanners = [
+        (pkgs.fetchFromGitHub {
+          owner = "ZeroQI";
+          repo = "Absolute-Series-Scanner";
+          rev = "33ce1de7199918371eff75e894d35d52bb9a6f30";
+          sha256 = "dqcAGAFPps0Hd18j54FTPGVH8KK0iYBfpI4Q3qRT+H0=";
+        })
+    ];
   };
 }
