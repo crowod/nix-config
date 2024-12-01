596c049753599bc0a9cff0119109575fa05b3d99
crowod <xiangcz929@gmail.com>
2024-12-02 00:17:40 +0800

    add pkg
    Signed-off-by: crowod <xiangcz929@gmail.com>

--- home/linux/hyprland/apps.nix
+++ home/linux/hyprland/apps.nix
@@ -1,18 +1,19 @@
-{ pkgs, ... }: {
+{ ... }: {
   programs = {
     # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
     google-chrome = {
       enable = true;
-      package = (pkgs.google-chrome.overrideAttrs (oldAttrs: {
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.wrapGAppsHook ];
-      }));
       commandLineArgs = [
-          "--enable-features=TouchpadOverscrollHistoryNavigation,WaylandWindowDecorations"
+          "--gtk-version=4"
           "--enable-wayland-ime"
+          "--ozone-platform-hint=auto"
           "--ozone-platform=wayland"
           "--wayland-per-window-scaling"
           "--wayland-text-input-version=3"
       ];
+    };
+    firefox = {
+      enable = true;
     };
   };
 }
